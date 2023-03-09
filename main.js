b0d45b702bb3334d2e66d65203f0b163d9915152
Dogan Celik <dogancelik86@gmail.com>
2023-03-09 16:53:43 +0100

    include.js update

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 setURL('https://gruppenarbeit-485join.developerakademie.net/join/smallest_backend_ever')
-
-let users =[]
+let user
+let users = []
 async function init() {
+    loadServer();
+    loadLocalStorage();
+}
+
+
+async function loadServer() {
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
+}
+
+async function loadLocalStorage(){
+ let loggedIn = localStorage.getItem('loggedIn')
+redrictToPage(loggedIn)
+}
+
+function redrictToPage(loggedIn){
+    if (loggedIn == true) {
+        window.location.href = summary.html
+    } else {
+        window.location.href = login.html
+    }
 }
