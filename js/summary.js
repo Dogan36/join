8324f4c15b70580352d8fa744eaed89858791c46
Christian Gewandt <116904949+ChristianGewandt@users.noreply.github.com>
2023-03-17 13:09:55 +0100

    summary2

---
+++ js/summary.js
@@ -0,0 +1,7 @@
+function onMouseOverImg(id, src) {
+    document.getElementById(id).setAttribute('src', src);
+}
+
+function onMouseOutImg(id, src) {
+    document.getElementById(id).setAttribute('src', src);
+}
