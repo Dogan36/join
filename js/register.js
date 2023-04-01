df06d40c1a5a543abd9ba1bcd34c6d0f5ae9960b
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2023-04-01 14:51:24 +0200

    implementet different error messages

--- js/register.js
+++ js/register.js
@@ -25,58 +25,99 @@ function listenerPasswordImg() {
     passwordToggle.addEventListener('click', togglePasswordVisibility);
 }
 
-function addUser() {
-    var hasError = false;
-    if (document.getElementById('registerName').value === '') {
-        document.getElementById('registerNameError').classList.remove('d-none');
-        hasError = true;
-    } else {
-        document.getElementById('registerNameError').classList.add('d-none');
-    }
-    if (document.getElementById('registerEmail').value === '') {
-        document.getElementById('registerEmailError').classList.remove('d-none');
-        hasError = true;
-    } else {
-        document.getElementById('registerEmailError').classList.add('d-none');
-    }
-    if (document.getElementById('registerPassword').value === '') {
-        document.getElementById('registerPasswordError').classList.remove('d-none');
-        hasError = true;
 
-    } else {
-        document.getElementById('registerPasswordError').classList.add('d-none');
+async function checkInputsSignUp() {
+    document.getElementById('registerEmailError').classList.add('d-none');
+    document.getElementById('registerEmailFormatError').classList.add('d-none');
+    document.getElementById('registerEmailTakenError').classList.add('d-none');
+    var hasError = false;
+    if (checkSignUpName()) {
+        hasError = true;
+    }
+    if (checkSignUpEmail()) {
+        hasError = true;
+    }
+    if (checkSignUpPassword()) {
+        hasError = true;
     }
     if (!hasError) {
-        document.getElementById("registerForm").submit();
-        window.location.href = "login.html?msg=TestMessage"
+        await addUser()
     }
 }
 
 
-function togglePasswordVisibility() {
-    if (passwordInput.value === '') {
-        passwordToggle.src = standartIcon;
+function checkSignUpName() {
+    if (document.getElementById('registerName').value === '') {
+        document.getElementById('registerNameError').classList.remove('d-none');
+        return true;
     } else {
-        if (passwordInput.type === 'password') {
-            passwordInput.type = 'text';
-            passwordToggle.src = visibleIcon;
-        } else {
-            passwordInput.type = 'password';
-            if (passwordToggle.src !== '/assets/img/loginPassword.svg') {
-                passwordToggle.src = unVisibleIcon;
-            }
+        document.getElementById('registerNameError').classList.add('d-none');
+    }
+}
+
+
+function checkSignUpEmail() {
+    var email = document.getElementById('registerEmail').value;
+
+    if (email === '') {
+        document.getElementById('registerEmailError').classList.remove('d-none');
+        return true;
+    }
+    if (email.indexOf('@') === -1) {
+        document.getElementById('registerEmailFormatError').classList.remove('d-none');
+        return true;
+    }
+
+    if (checkEmailExists(email)) {
+        document.getElementById('registerEmailTakenError').classList.remove('d-none');
+        return true;
+    }
+
+}
+
+function checkEmailExists(email) {
+    for (var i = 0; i < users.length; i++) {
+        if (users[i].email === email) {
+            return true;
         }
     }
 }
 
+    function checkSignUpPassword() {
+        if (document.getElementById('registerPassword').value === '') {
+            document.getElementById('registerPasswordError').classList.remove('d-none');
+            return true;
+        }
+        if (document.getElementById('registerPassword').value.length<7) {
+            document.getElementById('registerPasswordLengthError').classList.remove('d-none');
+            return true;
+        }
+    }
+    
+    function togglePasswordVisibility() {
+        if (passwordInput.value === '') {
+            passwordToggle.src = standartIcon;
+        } else {
+            if (passwordInput.type === 'password') {
+                passwordInput.type = 'text';
+                passwordToggle.src = visibleIcon;
+            } else {
+                passwordInput.type = 'password';
+                if (passwordToggle.src !== '/assets/img/loginPassword.svg') {
+                    passwordToggle.src = unVisibleIcon;
+                }
+            }
+        }
+    }
 
-function changePasswortImage() {
-    if (passwordInput.value === '') {
-        passwordToggle.src = standartIcon;
-    } else if (passwordInput.type == 'text') {
-        passwordToggle.src = visibleIcon;
+
+    function changePasswortImage() {
+        if (passwordInput.value === '') {
+            passwordToggle.src = standartIcon;
+        } else if (passwordInput.type == 'text') {
+            passwordToggle.src = visibleIcon;
+        }
+        else {
+            passwordToggle.src = unVisibleIcon
+        }
     }
-    else {
-        passwordToggle.src = unVisibleIcon
-    }
-}
