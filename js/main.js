6cc1481d77a7dd438e16fd73368314b61de605bd
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2023-04-12 12:53:57 +0200

    send mail function for forgotten password

--- js/main.js
+++ js/main.js
@@ -143,7 +143,7 @@ function checkEmail(element) {
       return
   }
   if (element == 'forgot'){
-      sendNewPasswordLink()
+    sendNewPasswordLink()
       return
   }
   checkPassword(`${element}`)
