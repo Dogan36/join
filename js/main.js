b077d26378f940d23f4e762ae4f603a774731d44
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2023-04-12 22:32:55 +0200

    bugfix confirmation message

--- js/main.js
+++ js/main.js
@@ -234,6 +234,27 @@ function checkIncorrectPassword(element) {
   return false
 }
 
+function sendNewPasswordLink() {
+  let email = document.getElementById('forgotEmail').value
+  const xhr = new XMLHttpRequest();
+  const url = '//gruppenarbeit-485join.developerakademie.net/join/send_mail.php';
+
+  xhr.open('POST', url, true);
+  xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      // Success!
+      console.log('Email sent!');
+    }
+  };
+
+  const message = `Hello,\n\nPlease click on the following link to reset your password: http://gruppenarbeit-485join.developerakademie.net/join/forgot.html?email=${email}\n\nBest regards,\nYour Join Team`;
+  const params = `name=Join&mail=noreply@join.com&message=${message}`;
+
+  xhr.send(params);
+  showConfirmation('login')
+}
+
 function checkSecondPassword() {
   const password = document.getElementById('resetPassword').value
   const confirmPassword = document.getElementById('resetConfirmPassword').value;
@@ -252,9 +273,6 @@ async function updatePassword() {
   user.password = newPassword;
   await setServer()
   showConfirmation('forgot')
-  setTimeout(function () {
-    window.location.href = "login.html";
-  }, 2000);
 }
 
 function showConfirmation(element) {
@@ -266,9 +284,13 @@ function showConfirmation(element) {
     confirmationElement.style.bottom = '50%'; // animate to top position
   }, 100);
 
-setTimeout(() => {
-  blackLayer.classList.add('d-none');
+  setTimeout(() => {
+    blackLayer.classList.add('d-none');
+    if(element=='forgot'){
+    window.location.href = "login.html";
+  } if(element =='login'){showContentLogin('loginContainer')}
 }, 1200);
+
 }
 
 
