f01d32e6331a4d8f0832cbc9054dd93625e5ecd0
Christian Gewandt <116904949+ChristianGewandt@users.noreply.github.com>
2023-04-17 21:06:03 +0200

    Task popup window in

--- js/board.js
+++ js/board.js
@@ -1,3 +1,20 @@
 function renderBoard() {
 
 }
+
+
+/**
+ * Opens the Add Task pop-up window in the board page. (Öffnet das Pop up Fenster Add Task in der board Seite.)
+ */
+function popUpWindowaddTask() {
+    document.getElementById('container-opened-task').classList.remove('d-none');
+    document.getElementById('add-task-window').classList.remove('d-none');
+}
+
+/**
+ * Closes the Add Task pop-up window in the board page. (Schließt das Pop up Fenster Add Task in der board Seite.)
+ */
+function popUpWindowCloseAddTask() {
+    document.getElementById('container-opened-task').classList.add('d-none');
+    document.getElementById('add-task-window').classList.add('d-none');
+}
