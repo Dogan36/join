d60a32ab6c298b1c3f3d4a33eb5f56270e8966c1
Christian Gewandt <116904949+ChristianGewandt@users.noreply.github.com>
2023-04-22 02:42:34 +0200

    style Assigned to checkbox

--- js/add_task.js
+++ js/add_task.js
@@ -586,13 +586,8 @@ async function addTaskJasonArray() {
   let TaskJasonArray = {
     'taskTitle': title,
     'taskDescription': description,
-<<<<<<< HEAD
     'taskCategory': category,
     'assingnedTo': selectedContacts,
-=======
-    'taskCategory': category[0],
-    'assingnedTo': assignedTo,
->>>>>>> aebfabc4b0f48d0348dcc4907303c804808c559e
     'dueDate': dueDate,
     'prio': addTaskSelectPrios,
     'subtasks': addTaskNewSubtasks,
