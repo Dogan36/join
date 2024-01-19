edadffb8e2f058700b7b4e826fb06fd87d0f44d9
Dogan36 <125744210+Dogan36@users.noreply.github.com>
2024-01-19 18:20:13 +0100

    js docs on add_task.js

--- js/add_task.js
+++ js/add_task.js
@@ -47,6 +47,12 @@ let prios = [
 let selectedColor;
 let n = 0
 
+/**
+ * This function checks the mandatory fields on add task an edit task
+ * 
+ * @param {number} indexOfEditedTask This is the index of the task to edit in tasks
+ * @returns boolean
+ */
 function checkMandatoryFields(indexOfEditedTask) {
   let errorCount = 0;
   errorCount += checkMandatoryFieldTitle() ? 1 : 0;
@@ -57,13 +63,18 @@ function checkMandatoryFields(indexOfEditedTask) {
   if (errorCount > 0) return;
   addTask(indexOfEditedTask)
 }
-
+/**
+ * This function adds the task and goes back to board page
+ */
 async function addTask() {
   selectContactToAssign()
-  await addTaskJsonArray(); /* Das addTaskJasonArray() hollt sich die restlichen Punkte aus den globalen Variablen   */
+  await addTaskJsonArray(); 
   goToBoardPage()
 }
 
+/**
+ * This function adds the task to the json array
+ */
 async function addTaskJsonArray() {
   let taskJsonArray = getAddTaskJson()
   if (n != 2) {
@@ -78,6 +89,11 @@ async function addTaskJsonArray() {
   renderBoard()
 }
 
+/**
+ * This function gets the json of the task
+ * 
+ * @returns json
+ */
 function getAddTaskJson() {
   let taskJsonArray =
   {
@@ -93,11 +109,16 @@ function getAddTaskJson() {
   return taskJsonArray
 }
 
+/**
+ * This function clears the input fields off add task and clears the warnings
+ */
 function clearTheInputFields() {
   deleteAddTaskFields()
   clearWarnings()
 }
-
+/**
+ * This function deletes the values of the input fields and resets the buttons and dropdowns
+ */
 function deleteAddTaskFields() {
   document.getElementById(`task-title-input${n}`).value = '';
   document.getElementById(`add-task-description${n}`).value = '';
@@ -108,9 +129,16 @@ function deleteAddTaskFields() {
   renderAddTaskDropdowns()
 }
 
+/**
+ * This function resets the subtasks
+ */
 function resetSubtasks() {
   subtasks = []
 }
+
+/**
+ * This function clears the warnings
+ */
 
 function clearWarnings() {
   clearTitleWarnings()
@@ -120,33 +148,51 @@ function clearWarnings() {
   clearDueDateWarnings()
   clearPrioWarnings()
 }
+/**
+ * This function adds a warning on title
+ */
 
 function addTitleWarnings() {
   document.getElementById(`required-title${n}`).classList.remove('hidden');
   document.getElementById(`task-title-input${n}`).style.border = '2px solid red';
 }
 
+/**
+ * This function clears the warning on title
+ */
 function clearTitleWarnings() {
   document.getElementById(`required-title${n}`).classList.add('hidden');
   document.getElementById(`task-title-input${n}`).style = '';
 }
 
+/**
+ * This function adds warning on description
+ */
 function addDescriptionWarnings() {
   document.getElementById(`required-description${n}`).classList.remove('hidden');
   document.getElementById(`add-task-description${n}`).style.border = '2px solid red';
 }
 
+/**
+ * This function clears warning on description
+ */
 function clearDescriptionWarnings() {
   document.getElementById(`required-description${n}`).classList.add('hidden');
   document.getElementById(`add-task-description${n}`).style = '';
 }
 
+/**
+ * This function adds warning on new category
+ */
 function addNewCategoryWarning() {
   document.getElementById(`new-category-container${n}`).style.border = '2px solid red';
   document.getElementById(`required-category${n}`).classList.remove('hidden');
   document.getElementById(`required-category${n}`).innerHTML = 'Please enter name and color'
 }
 
+/**
+ * This function adds warning on new priority
+ */
 function addPrioWarnings() {
   document.getElementById(`prio-red${n}`).style.border = '2px solid red';
   document.getElementById(`prio-yellow${n}`).style.border = '2px solid red';
@@ -154,6 +200,9 @@ function addPrioWarnings() {
   document.getElementById(`required-prio${n}`).classList.remove('hidden');
 }
 
+/**
+ * This function clears warning on new priority
+ */
 function clearPrioWarnings() {
   document.getElementById(`prio-red${n}`).style = '';
   document.getElementById(`prio-yellow${n}`).style = '';
@@ -161,13 +210,18 @@ function clearPrioWarnings() {
   document.getElementById(`required-prio${n}`).classList.add('hidden');
 }
 
+/**
+ * This function adds warning on category
+ */
 function addCategoryWarnings() {
   document.getElementById(`required-category${n}`).classList.remove('hidden');
   document.getElementById(`required-category${n}`).innerHTML = 'This field is required'
   document.getElementById(`select-container${n}`).style.border = '2px solid red';
-
 }
 
+/**
+ * This function clears warning on category
+ */
 function clearCategoryWarnings() {
   document.getElementById(`required-category${n}`).classList.add('hidden');
   document.getElementById(`select-container${n}`).style = '';
@@ -175,26 +229,43 @@ function clearCategoryWarnings() {
   document.getElementById(`new-category-container${n}`).style = '';
 }
 
+/**
+ * This function adds warning on assigned to
+ */
 function addAssignedTowarnings() {
   document.getElementById(`required-assigned-to${n}`).classList.remove('hidden');
   document.getElementById(`assigned-to-container${n}`).style.border = '2px solid red';
 }
 
+/**
+ * This function clears warning on assigned to
+ */
 function clearAssignedTowarnings() {
   document.getElementById(`required-assigned-to${n}`).classList.add('hidden');
   document.getElementById(`assigned-to-container${n}`).style = '';
 }
 
+/**
+ * This function adds warning on due date
+ */
 function addDueDateWarnings() {
   document.getElementById(`required-due-date${n}`).classList.remove('hidden');
   document.getElementById(`due-date${n}`).style.border = '2px solid red';
 }
 
+/**
+ * This function clears warning on due date
+ */
 function clearDueDateWarnings() {
   document.getElementById(`required-due-date${n}`).classList.add('hidden');
   document.getElementById(`due-date${n}`).style = '';
 }
 
+/**
+ * This function chooses priority
+ * 
+ * @param {string} color This is the color of the choosen priority
+ */
 function choosePrio(color) {
   clearPrioWarnings()
   let divClicked = document.getElementById(`${color}${n}`)
@@ -203,20 +274,31 @@ function choosePrio(color) {
   divClicked.classList.add(`${color}`)
   changePrioButtonIcon(color)
 }
-
+/**
+ * This function sets the prio depending on choosen color
+ * 
+ * @param {string} element his is the color of the choosen priority
+ */
 function setPrioColor(element) {
   if (element == `prio-red`) prio = prios[2]
   if (element == `prio-yellow`) prio = prios[1]
   if (element == `prio-green`) prio = prios[0]
 }
 
+/**
+ * This function changes the source of the img an choosen buttons for design reasons
+ * 
+ * @param {string} element This is the choosen color
+ */
 function changePrioButtonIcon(element) {
-  console.log(element)
   if (element == `prio-red`) document.getElementById(`prio-urgent-icon${n}`).src = 'assets/img/prio-urgent-white-icon.svg'
   if (element == `prio-yellow`) document.getElementById(`prio-medium-icon${n}`).src = 'assets/img/prio-medium-white-icon.svg'
   if (element == `prio-green`) document.getElementById(`prio-low-icon${n}`).src = 'assets/img/prio-low-white-icon.svg'
 }
 
+/**
+ * This function resets the prio buttons
+ */
 function resetPrioButtons() {
   document.getElementById(`prio-red${n}`).classList.remove('prio-red')
   document.getElementById(`prio-yellow${n}`).classList.remove('prio-yellow')
@@ -226,6 +308,11 @@ function resetPrioButtons() {
   document.getElementById(`prio-low-icon${n}`).src = 'assets/img/prio-low-icon.svg'
 }
 
+/**
+ * This function checks if the input title is empty and adds warning otherwise clears the warnings
+ * 
+ * @returns boolean
+ */
 function checkMandatoryFieldTitle() {
   let inputFeldTitle = document.getElementById(`task-title-input${n}`);
   if (inputFeldTitle.value === '') {
@@ -235,6 +322,11 @@ function checkMandatoryFieldTitle() {
   else clearTitleWarnings()
 }
 
+/**
+ * This function checks if the input description is empty and adds warning otherwise clears the warnings
+ * 
+ * @returns boolean
+ */
 function checkMandatoryFieldDescription() {
   let textareaFeldDescription = document.getElementById(`add-task-description${n}`);
   if (textareaFeldDescription.value === '') {
@@ -244,6 +336,11 @@ function checkMandatoryFieldDescription() {
   else clearDescriptionWarnings()
 }
 
+/**
+ * This function checks if the input due date is empty and adds warning otherwise clears the warnings
+ * 
+ * @returns boolean
+ */
 function checkMandatoryFieldDueDate() {
   let inputFeldDueDate = document.getElementById(`due-date${n}`);
   if (inputFeldDueDate.value === '') {
@@ -253,8 +350,12 @@ function checkMandatoryFieldDueDate() {
   else clearDueDateWarnings()
 }
 
+/**
+ * This function checks if the input category is empty and adds warning otherwise clears the warnings
+ * 
+ * @returns boolean
+ */
 function checkMandatoryFieldCategory() {
-
   if (choosenCategory == undefined) {
     addCategoryWarnings();
     return true
@@ -262,6 +363,11 @@ function checkMandatoryFieldCategory() {
   else clearCategoryWarnings()
 }
 
+/**
+ * This function checks if the input prio is empty and adds warning otherwise clears the warnings
+ * 
+ * @returns boolean
+ */
 function checkPrio() {
   let prioColorRed = document.getElementById(`prio-red${n}`);
   let prioColorYellow = document.getElementById(`prio-yellow${n}`);
@@ -275,6 +381,9 @@ function checkPrio() {
   }
 }
 
+/**
+ * This function renders the categories into the drop down
+ */
 function renderAddTaskCategorys() {
   let content = document.getElementById(`categorySelection${n}`);
   content.innerHTML = ''
@@ -284,6 +393,13 @@ function renderAddTaskCategorys() {
   }
 }
 
+/**
+ * This function generates the html of the categories
+ * 
+ * @param {number} i index of the category in categorys
+ * @param {string} category name of the category
+ * @returns string
+ */
 function renderAddTaskCategoriesHTML(i, category) {
   return `
   <div class="categoryOption" >
@@ -297,6 +413,9 @@ function renderAddTaskCategoriesHTML(i, category) {
   </div>`
 }
 
+/**
+ * This function renders the category which is selected
+ */
 function renderAddTaskCategorySelect() {
   let content = document.getElementById(`select-container${n}`);
   content.innerHTML = `
@@ -311,37 +430,53 @@ function renderAddTaskCategorySelect() {
 </div>`
 }
 
+/**
+ * This function toogles the category container
+ */
 function toggleAddTaskCategory() {
   if (document.getElementById(`content-category-container${n}`).classList.contains('d-none'))
     openAddTaskCategory()
   else closeAddTaskCategory()
 }
 
+/**
+ * This function opens the category container
+ */
 function openAddTaskCategory() {
   document.getElementById(`content-category-container${n}`).classList.remove('d-none')
   document.getElementById(`arrowIconCategory${n}`).classList.add('arrow-rotate')
 }
 
+/**
+ * This function closes the category container
+ */
 function closeAddTaskCategory() {
   document.getElementById(`content-category-container${n}`).classList.add('d-none')
   document.getElementById(`arrowIconCategory${n}`).classList.remove('arrow-rotate')
   choosenCategory = undefined
   checkMandatoryFieldCategory()
 }
-
+/**
+ * This function opens the new category input
+ */
 function openNewCategoryInput() {
   choosenCategory = undefined
   document.getElementById(`new-category-container${n}`).classList.remove('d-none');
   document.getElementById(`color-container${n}`).classList.remove('d-none');
   document.getElementById(`select-container${n}`).classList.add('d-none');
 }
-
+/**
+ * This function closes the new category input
+ */
 function closeAddTaskNewCategory() {
   document.getElementById(`new-category-container${n}`).classList.add('d-none');
   document.getElementById(`color-container${n}`).classList.add('d-none');
   document.getElementById(`select-container${n}`).classList.remove('d-none');
 }
 
+/**
+ * This function checks if the new category name is inserted and color choosen and shows warning
+ */
 function checkNewCategoryName() {
   let name = document.getElementById(`category${n}`);
   let color = document.getElementById(`color-button-container${n}`);
@@ -355,6 +490,9 @@ function checkNewCategoryName() {
   }
 }
 
+/**
+ * This function adds new category to categorys
+ */
 async function addNewCategory() {
   let categorytext = document.getElementById(`category${n}`);
   let categoryID = categorys.length
@@ -367,11 +505,19 @@ async function addNewCategory() {
   await setItem('categorys', categorys);
 }
 
+/**
+ * This function resets inputs on new category
+ */
 function resetNewCategoryInput() {
   document.getElementById(`category${n}`).value = "";
   document.getElementById(`color-button-container${n}`).innerHTML = '';
 }
 
+/**
+ * This function deletes category from categorys 
+ * 
+ * @param {number} i index of category in categorys
+ */
 async function deleteCategory(i) {
   categorys.splice(i, 1);
   await setItem('categorys', categorys);
@@ -379,18 +525,34 @@ async function deleteCategory(i) {
   toggleAddTaskCategory()
 }
 
+/**
+ * This function renders the selected colour to color button container
+ * 
+ * @param {number} i index of color in buttonBackgroundColor
+ */
 function colorButton(i) {
   selectedColor = '';
   selectedColor = buttonBackgroundColor[i];
   document.getElementById(`color-button-container${n}`).innerHTML = `<div class="color-category-button" style="background-color :${selectedColor};"></div>`;
 }
 
+/**
+ * This function renders the selected category to select container
+ * 
+ * @param {number} i index of category in categorys
+ */
 function selectCategory(i) {
   clearCategoryWarnings()
   choosenCategory = categorys[i];
   document.getElementById(`select-container${n}`).innerHTML = categoryTemplate(choosenCategory)
 }
 
+/**
+ * This function generates the html for the select container
+ * 
+ * @param {JSON} category This is the json of the selected category
+ * @returns string
+ */
 function categoryTemplate(category) {
   return `
     <div onclick="renderAddTaskCategorySelect(); renderAddTaskCategorys(), toggleAddTaskCategory()" class="categoryOption">
@@ -402,7 +564,9 @@ function categoryTemplate(category) {
                   <img class="arrow-icon" id="arrowIconCategory${n}" src="./assets/img/arrow_icon.svg" alt="">
                 </div>`
 }
-
+/**
+ * This function generates the html for the selected contacts
+ */
 function renderAddTaskContactsSelect() {
   document.getElementById(`select-contacts-container${n}`).innerHTML = `
   <div onclick="toggleAddTaskContacts()" class="contactsOption">
@@ -416,6 +580,9 @@ function renderAddTaskContactsSelect() {
           `
 }
 
+/**
+ * This function generates the html for the contacts
+ */
 function renderAddTaskContacts() {
   let content = document.getElementById(`contacts${n}`);
   content.innerHTML = '';
@@ -440,35 +607,49 @@ function renderAddTaskContacts() {
   }
 }
 
+/**
+ * This function toogles the add task contacts dropdown
+ */
 function toggleAddTaskContacts() {
   if (document.getElementById(`contacts${n}`).classList.contains('d-none'))
     openAddTaskContacts()
   else closeAddTaskContacts()
 }
 
+/**
+ * This function opens the add task contacts dropdown
+ */
 function openAddTaskContacts() {
   document.getElementById(`contacts${n}`).classList.remove('d-none')
   document.getElementById(`arrow-rotate${n}`).classList.add('arrow-rotate')
 }
 
+/**
+ * This function closes the add task contacts dropdown
+ */
 function closeAddTaskContacts() {
   document.getElementById(`contacts${n}`).classList.add('d-none')
   document.getElementById(`arrow-rotate${n}`).classList.remove('arrow-rotate')
-
 }
-
+/**
+ * This function opens the invite new contact container
+ */
 function openInviteNewContact() {
   document.getElementById(`select-contacts-container${n}`).classList.add('d-none');
   document.getElementById(`assigned-to-container${n}`).classList.remove('d-none');
 }
 
+/**
+ * This function clears and closes the invite new contact container
+ */
 function closeInviteNewContact() {
   clearInviteNewContactValue()
-  document.getElementById(`invite-new-contact${n}`).value = ''
   document.getElementById(`select-contacts-container${n}`).classList.remove('d-none');
   document.getElementById(`assigned-to-container${n}`).classList.add('d-none');
 }
-
+/**
+ * This function checks if new contact field is empty and adds warning otherwise adds invited contact
+ */
 function checkNewContactField() {
   let newContactField = document.getElementById(`invite-new-contact${n}`);
   if (newContactField.value.indexOf('@') === -1) {
@@ -479,6 +660,9 @@ function checkNewContactField() {
   }
 }
 
+/**
+ * This function adds invited contact to contacts
+ */
 async function addInviteContact() {
   let email = document.getElementById(`invite-new-contact${n}`);
   let name = email.value.split('@')[0];
@@ -492,10 +676,16 @@ async function addInviteContact() {
   openAddTaskContacts()
 }
 
+/**
+ * This function clears invite new contact input
+ */
 function clearInviteNewContactValue() {
   document.getElementById(`invite-new-contact${n}`).value = ''
 }
 
+/**
+ * This funtion checks the checkboxes of contacts and adds them to selectedContacts
+ */
 function selectContactToAssign() {
   selectedContacts = []
   let checkboxes = document.getElementsByName(`option[]${n}`);
@@ -506,13 +696,20 @@ function selectContactToAssign() {
   }
 }
 
+/**
+ * This funtion uncheckes all checkboxes of contacts
+ */
 function uncheckSelectedContacts() {
   let checkboxes = document.getElementsByName(`option[]${n}`);
   for (let i = 0; i < checkboxes.length; i++) {
     checkboxes[i].checked = false
   }
 }
-
+/**
+ * This function gets the current date
+ * 
+ * @returns string
+ */
 function getCurrentDate() {
   var today = new Date();
   var year = today.getFullYear();
@@ -521,20 +718,30 @@ function getCurrentDate() {
   return year + '-' + month + '-' + day;
 }
 
+/**
+ * This function sets the minimum of due date to current date
+ */
 function setCurrentDate() {
   document.getElementById(`due-date${n}`).setAttribute('min', getCurrentDate());
 }
 
+/**
+ * This function opens the new subtask container
+ */
 function openSubtask() {
   document.getElementById(`add-new-subtask-container${n}`).classList.add('d-none');
   document.getElementById(`new-subtask-container${n}`).classList.remove('d-none');
 }
-
+/**
+ * This function closes the new subtask container
+ */
 function closeSubtask() {
   document.getElementById(`add-new-subtask-container${n}`).classList.remove('d-none');
   document.getElementById(`new-subtask-container${n}`).classList.add('d-none');
 }
-
+/**
+ * This function checks if new subtask input isn´t empty an adds new subtask to subtasks
+ */
 function addNewSubtask() {
   let newSubtaskInput = document.getElementById(`new-subtask-point${n}`);
   if (newSubtaskInput.value !== '') {
@@ -548,6 +755,9 @@ function addNewSubtask() {
   }
 }
 
+/**
+ * This function renders html to subtask container
+ */
 function renderSubtasks() {
   let subtasksContainer = document.getElementById(`add-task-subtask-point${n}`);
   subtasksContainer.innerHTML = '';
@@ -569,6 +779,9 @@ function renderSubtasks() {
   }
 }
 
+/**
+ * This function directs user to board page
+ */
 function goToBoardPage() {
   let boardButton = document.getElementById('board');
   renderBoard()
